356565a0b35ee31358cc4df22fca1bf29cdf5223
Beibarys - totKtoXam <kham.b.13.09@gmail.com>
2021-10-07 15:32:37 +0600

    namespace EntityAggregates with states, handlers and handlers chain factory

---
+++ src/Template.Shared/EntityAggregates/States.Abstractions/IAggregate.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Template.Shared.EntityAggregates.States.Abstractions
+{
+    /// <summary>
+    /// Aggregate Interface.
+    /// </summary>
+    public interface IAggregate
+    {
+    }
+}
---
+++ src/Template.Shared/EntityAggregates/States.Abstractions/IRowVersionAggregate.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Template.Shared.EntityAggregates.States.Abstractions
+{
+    /// <summary>
+    /// Row Version Aggregate Interface.
+    /// </summary>
+    /// <seealso cref="Template.Shared.EntityAggregates.States.Abstractions.IAggregate" />
+    public interface IRowVersionAggregate : IAggregate
+    {
+        /// <summary>
+        /// Gets or sets the row version.
+        /// </summary>
+        /// <value>
+        /// The row version.
+        /// </value>
+        [Timestamp]
+        byte[] RowVersion { get; set; }
+    }
+}
